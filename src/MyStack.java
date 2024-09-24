750c6f29f1e90d033939faa503206356139773d7
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2024-09-24 08:05:29 -0500

    yay

---
+++ src/MyStack.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class MyStack<T>
+{
+    private ArrayList<T> stack;
+
+    public void push(E o);
+    public E peek();
+    public E pop();
+    public E get(int x);
+    public int size();
+    public boolean isEmpty();
+    public void clear();
+}
---
+++ src/StackInterface.java
@@ -0,0 +1,11 @@
+import
+public interface StackInterface<E>
+{
+    public void push(E o);
+    public E peek();
+    public E pop();
+    public E get(int x);
+    public int size();
+    public boolean isEmpty();
+    public void clear();
+}
