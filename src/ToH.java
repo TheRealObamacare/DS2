aa2ccdeba6836220a85daa54a85872896bd48168
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2024-09-30 08:04:51 -0500

    finish isWinner method pls

--- src/ToH.java
+++ src/ToH.java
@@ -16,28 +16,33 @@ public class ToH
                 break;
             else
                 System.out.println("That value is out of range, please try again.\nEnter the number of disks you would like to play with (3-6):");
-        }while();
+        }while(true);
         for(int i = x; i > 0; i--)
             yay.pole1.push(i);
+        zeros(yay.pole1, yay.pole2, yay.pole3, x+2);
         printPoles(yay.pole1, yay.pole2, yay.pole3, x);
+        do {
+
+        }while(isWinner(yay.pole3))
+    }
+    public static void zeros(StackInterface<Integer> pole1,StackInterface<Integer> pole2, StackInterface<Integer> pole3, int x)
+    {
+        for(int i = x - pole1.size(); i > 0; i--)
+            pole1.push(0);
+        for(int i = pole1.size(); i > 0; i--)
+            pole2.push(0);
+        for(int i = pole1.size(); i > 0; i--)
+            pole3.push(0);
     }
     public static void printPoles(StackInterface<Integer> pole1,StackInterface<Integer> pole2, StackInterface<Integer> pole3, int x)
     {
-        System.out.printf(pole1.toString() + pole2.toString() + pole3.toString());
-        for(int i = x; i > 0; i++)
+        String s = "";
+        for(int i = x - 1; i >= 0; i--)
         {
-            System.out.println(printHelper(pole1, i) + printHelper(pole2, i) + printHelper(pole3, i));
+            s = (printHelper(pole1, i) + printHelper(pole2, i) + printHelper(pole3, i));
+            System.out.println(s);
         }
-        //25 dashes System.out.println("-------------------------");
-    }
-    public static void setUp(StackInterface<Integer> pole1,StackInterface<Integer> pole2, StackInterface<Integer> pole3, int x)
-    {
-        for(int i = x - pole1.size(); i > 0; i++)
-            pole1.push(0);
-        for(int i = x - pole2.size(); i > 0; i++)
-            pole1.push(0);
-        for(int i = x - pole3.size(); i > 0; i++)
-            pole1.push(0);
+        System.out.println("-".repeat(s.length()));
     }
     public static String printHelper(StackInterface<Integer> pole, int i)
     {
@@ -45,21 +50,13 @@ public class ToH
         String disk = "";
         if(pole.get(i) == 0)
             disk = "-";
-        else if(pole.get(i) == 1)
-            disk = "XXX";
-        else if(pole.get(i) == 2)
-            disk = "XXXXX";
-        else if(pole.get(i) == 3)
-            disk = "XXXXXXX";
-        else if(pole.get(i) == 4)
-            disk = "XXXXXXXXX";
-        else if(pole.get(i) == 5)
-            disk = "XXXXXXXXXXX";
-        else if(pole.get(i) == 6)
-            disk = "XXXXXXXXXXXXX";
-        for(int x = 6 - pole.get(i); x > 0; x--)
-        {
-            spaces+=" ";
-        }
+        else
+            disk = "X".repeat(pole.get(i)*2+1);
+        spaces = " ".repeat(6 - pole.get(i));
+        return spaces + disk + spaces;
+    }
+    public static void isWinner()
+    {
+
     }
 }
